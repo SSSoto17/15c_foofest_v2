8072884305980367af9344f64c06ad38a6a423a5
Sif Melkane <siha0004@stud.kea.dk>
2025-01-07 14:53:33 +0100

    adjusted spacing and styling on headings

--- src/app/lineup/artists/single/[slug]/page.jsx
+++ src/app/lineup/artists/single/[slug]/page.jsx
@@ -12,7 +12,7 @@ export default async function ArtistSingle({ params }) {
   const artistImg = artist.logo.startsWith("https://") ? artist.logo : `${endpoint}/logos/${artist.logo}`;
 
   return (
-    <main>
+    <main className="my-8">
       <Link href="/lineup/artists">
         <p className="flex gap-4 items-center mb-4 text-aztec-300">
           <span className="">
@@ -25,20 +25,20 @@ export default async function ArtistSingle({ params }) {
       <section className="grid md:grid-cols-2 gap-10">
         <div>
           <Image src={picture} alt={`Image of ${artist.name}`} width={400} height={400} className="h-full w-full object-cover"></Image>
-          {artist.logoCredits && <small className="mt-2 inline-block body-copy-small">Photo by Johan von Bülow</small>}
+          {artist.logoCredits && <small className="mt-2 inline-block body-copy-small text-aztec-300">Photo by Johan von Bülow</small>}
         </div>
-        <article className="">
+        <article className=" pb-8">
           <h1 className="heading-tagline px-4 py-2 border-2 inline-block">
             Thursday<span className="ml-8">14.00</span>
           </h1>
-          <h2 className="heading-1 my-6">{artist.name}</h2>
-          <div className="grid grid-cols-2">
+          <h2 className="heading-2 my-6">{artist.name}</h2>
+          <div className="grid grid-cols-2 pb-8">
             <article>
-              <h3 className="heading-4">Genre</h3>
+              <h3 className="heading-7 text-for">Genre</h3>
               <p>{artist.genre}</p>
             </article>
             <article>
-              <h3 className="heading-4">Members</h3>
+              <h3 className="heading-7">Members</h3>
               <ul className="flex flex-wrap gap-x-3">
                 {artist.members.map((member) => (
                   <li>{member}</li>
@@ -48,7 +48,7 @@ export default async function ArtistSingle({ params }) {
             </article>
           </div>
           <article>
-            <h3 className="heading-4">About</h3>
+            <h3 className="heading-7">About</h3>
             <p>{artist.bio}</p>
           </article>
         </article>
